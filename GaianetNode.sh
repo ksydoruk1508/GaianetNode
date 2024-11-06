6c59eb41798849b498c269269d26fced798e7f24
Andante <84418705+ksydoruk1508@users.noreply.github.com>
2024-11-06 16:19:52 +0200

    Update GaianetNode.sh

--- GaianetNode.sh
+++ GaianetNode.sh
@@ -126,7 +126,7 @@ User=root
 [Install]
 WantedBy=multi-user.target
 EOF
-    sudo gaianet stop
+    gaianet stop
     sudo systemctl daemon-reload
     sudo systemctl enable gaianet.service
     sudo systemctl restart gaianet.service
